ae43e077093a791ef6f9891fa885a878ffcc085b
Gustavo Valentino <gusvalli95@gmail.com>
2025-11-16 13:30:05 -0300

    feat: show cancelled bookings as cancelled on bookings page

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -28,6 +28,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "./ui/alert-dialog";
+import { Booking } from "../generated/prisma/client";
 
 interface BookingItemProps {
   booking: {
@@ -48,6 +49,15 @@ interface BookingItemProps {
   };
 }
 
+const getStatus = (booking: Pick<Booking, "date" | "cancelled">) => {
+  if (booking.cancelled) {
+    return "cancelled";
+  }
+  const date = new Date(booking.date);
+  const now = new Date();
+  return date >= now ? "confirmed" : "finished";
+};
+
 const BookingItem = ({ booking }: BookingItemProps) => {
   const [sheetIsOpen, setSheetIsOpen] = useState(false);
 
@@ -67,9 +77,7 @@ const BookingItem = ({ booking }: BookingItemProps) => {
     executeCancelBooking({ bookingId: booking.id });
   };
 
-  const date = new Date(booking.date);
-  const now = new Date();
-  const status = !booking.cancelled && date >= now ? "confirmed" : "finished";
+  const status = getStatus(booking);
   const isConfirmed = status === "confirmed";
   return (
     <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
@@ -83,7 +91,11 @@ const BookingItem = ({ booking }: BookingItemProps) => {
                   : "bg-muted text-muted-foreground uppercase"
               }
             >
-              {status === "confirmed" ? "Confirmado" : "Finalizado"}
+              {status === "confirmed"
+                ? "Confirmado"
+                : status === "finished"
+                  ? "Finalizado"
+                  : "Cancelado"}
             </Badge>
 
             <div className="flex flex-col gap-2">
@@ -99,13 +111,13 @@ const BookingItem = ({ booking }: BookingItemProps) => {
 
           <div className="flex h-full w-[106px] flex-col items-center justify-center border-l py-3">
             <p className="text-xs capitalize">
-              {date.toLocaleDateString("pt-BR", { month: "long" })}
+              {booking.date.toLocaleDateString("pt-BR", { month: "long" })}
             </p>
             <p className="text-2xl">
-              {date.toLocaleDateString("pt-BR", { day: "2-digit" })}
+              {booking.date.toLocaleDateString("pt-BR", { day: "2-digit" })}
             </p>
             <p className="text-xs">
-              {date.toLocaleTimeString("pt-BR", {
+              {booking.date.toLocaleTimeString("pt-BR", {
                 hour: "2-digit",
                 minute: "2-digit",
               })}
@@ -170,7 +182,7 @@ const BookingItem = ({ booking }: BookingItemProps) => {
             <div className="text-muted-foreground flex items-center justify-between text-sm">
               <p>Data</p>
               <p>
-                {date.toLocaleDateString("pt-BR", {
+                {booking.date.toLocaleDateString("pt-BR", {
                   day: "2-digit",
                   month: "long",
                 })}
@@ -179,7 +191,7 @@ const BookingItem = ({ booking }: BookingItemProps) => {
             <div className="text-muted-foreground flex items-center justify-between text-sm">
               <p>Horário</p>
               <p>
-                {date.toLocaleTimeString("pt-BR", {
+                {booking.date.toLocaleTimeString("pt-BR", {
                   hour: "2-digit",
                   minute: "2-digit",
                 })}
